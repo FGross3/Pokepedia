40d41fdd7c275c2893433c3802d0b1fefbb890df
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-02-27 21:21:09 -0700

    Complete pokemon + typing cvs table

---
+++ Sequelize/config/connections.js
@@ -0,0 +1,14 @@
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize("starwars", "root", "Areyoureadykids?", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+module.exports = sequelize;
