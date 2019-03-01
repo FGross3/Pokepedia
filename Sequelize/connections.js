40efd50f647ede93b51ae4f0829355d3f3182f7f
Jorge Arturo Bradic <abradic95@email.arizona.edu>
2019-02-28 21:30:26 -0700

    Sequelize files and databases ready for mySQL deployment

--- Sequelize/connections.js
+++ Sequelize/connections.js
@@ -1,6 +1,6 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("starwars", "root", "Areyoureadykids?", {
+var sequelize = new Sequelize("pokemon_db", "root", "Areyoureadykids?", {
   host: "localhost",
   port: 3306,
   dialect: "mysql",
