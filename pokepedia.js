0170e2537333d4a6765d0f6027b668f07bdcaf58
majolex <majolex@matts-MacBook-Air.local>
2019-03-02 13:04:28 -0700

    added sign up

--- pokepedia.js
+++ pokepedia.js
@@ -8,3 +8,12 @@ window.onclick = function (event) {
         modal.style.display = "none";
     }
 }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
