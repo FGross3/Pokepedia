46343f850b389df88421e1cb067298078954fe1f
FGross <44384879+FGross3@users.noreply.github.com>
2019-03-08 22:44:56 -0700

    images displaying correctly

--- public/js/search.js
+++ public/js/search.js
@@ -1,13 +1,35 @@
 $(document).ready(function() {
 
+  // function renderCard() {
+    
+  //   $("#stats").append(`<h3><strong>Pokedex Number:</strong> <span id="id">${data.id}</span></h3>`);
+  //           $("#stats").append(`<h3><strong>Pokemon Name:</strong> <span id="name">${data.name}</span></h3>`);
+  //           $("#stats").append(`<h3><strong>Type 1:</strong> <span id="type1">${data.type1}</span></h3>`);
+  //           // $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${pokemon.type2}</span></h3>`);
+  //           if (!data.type2){
+  //             $("type2").hide()
+  //           } else{
+  //             $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${data.type2}</span></h3>`);
+  //           }
+  // }
+
 
     $.get("/api/pokemon", function(data) {
-        data.forEach(function (pokemon) {
-            console.log(pokemon);
-            $("#stats").append(`<h3><strong>Pokedex Number:</strong> <span id="id">${pokemon.id}</span></h3>`);
-            $("#stats").append(`<h3><strong>Pokemon Name:</strong> <span id="name">${pokemon.name}</span></h3>`);
-            $("#stats").append(`<h3><strong>Type 1:</strong> <span id="type1">${pokemon.type1}</span></h3>`);
-            $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${pokemon.type2}</span></h3>`);
+        data.forEach(function (data) {
+            var imagePath = "/images/";
+            console.log(data);
+            $("#stats").append(`<h3><strong>Pokemon Name:</strong> <span id="name">${data.name}</span></h3>`);
+            $("#stats").append(`<h3><strong>Pokedex Number:</strong> <span id="id">${data.id}</span></h3>`);
+            $("#stats").append(`<h3><strong>Pokemon Name:</strong> <span id="name">${data.name}</span></h3>`);
+            $("#stats").append(`<h3><strong>Type 1:</strong> <span id="type1">${data.type1}</span></h3>`);
+            if (!data.type2){
+              $("type2").hide()
+              $("#stats").append(`<span id="image"><img src=${imagePath}${data.images}></span>`);
+            } else{
+              $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${data.type2}</span></h3>`);
+              $("#stats").append(`<span id="image"><img src=${imagePath}${data.images}></span>`);
+            }
+            // $("#stats").append(`<span id="image"><img src=${imagePath}${pokemon.image}></span>`);
         })
     })
 
@@ -22,22 +44,23 @@ $(document).ready(function() {
   
         $.get("/api/pokemon/" + searchedPokemon, function(data) {
           console.log(data);
+          var imagePath = "/images/";
           if (data) {
             $("#stats").empty();
-            $("#stats").append(`<h3><strong>Pokedex Number:</strong> <span id="id">${data.id}</span></h3>`);
             $("#stats").append(`<h3><strong>Pokemon Name:</strong> <span id="name">${data.name}</span></h3>`);
+            $("#stats").append(`<h3><strong>Pokedex Number:</strong> <span id="id">${data.id}</span></h3>`);
             $("#stats").append(`<h3><strong>Type 1:</strong> <span id="type1">${data.type1}</span></h3>`);
-            $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${data.type2}</span></h3>`);
-            
-            // $("#id").text(data.id);
-            // $("#name").text(data.name);
-            // $("#type1").text(data.type1);
-            // $("#type2").text(data.type2);
+            if (!data.type2){
+              $("type2").hide()
+              $("#stats").append(`<span id="image"><img src=${imagePath}${data.images}></span>`);
+            } else{
+              $("#stats").append(`<h3><strong>Type 2:</strong> <span id="type2">${data.type2}</span></h3>`);
+              $("#stats").append(`<span id="image"><img src=${imagePath}${data.images}></span>`);
+            }
           }
           else {
-            $("#name").text("Your pokemon was not found.");
-            $("#stats").hide();
-            $("#error").show();
+            $("#stats").empty();
+            $("#stats").append(`<h3><strong>Your Pokemon cannot be found</strong> <span id="name"></span></h3>`);
           }
         });
 
